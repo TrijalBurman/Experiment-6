a496517b32541c9b2a7d45633027e8479f392641
TrijalBurman <62022917+TrijalBurman@users.noreply.github.com>
2024-08-05 15:17:32 +0530

    Add files via upload

---
+++ exp6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int array1[5] = {19,10,8,17,9};
+    int array2[] = {19,10,8,17,9};
+    int array3[5] = {19,10,8};
+
+    cout <<"\n Traditional Method: "
+    for(int i = 0; i < 5; ++i) {
+        
+    }
+}
